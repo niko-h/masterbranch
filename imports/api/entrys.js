d85d24cde9a8faea82a97dd46269c5c47540c656
Niko <cvs@nikolaushoefer.de>
2017-03-01 13:08:47 +0100

    Moved Email-config to /server/main.js

--- imports/api/entrys.js
+++ imports/api/entrys.js
@@ -7,51 +7,6 @@ export const Entrys = new Mongo.Collection('entrys');
 if (Meteor.isServer) {
   // This code only runs on the server
 
-  Meteor.startup(() => {
-    smtp = {
-      username: 'post@nikolaushoefer.de',   // eg: server@gentlenode.com
-      password: 'Bmbhime172342',   // eg: 3eeP1gtizk5eziohfervU
-      server:   'smtp.mailbox.org',  // eg: mail.gandi.net
-      port: 25
-    }
-
-    process.env.MAIL_URL = 'smtp://' + encodeURIComponent(smtp.username) + ':' + encodeURIComponent(smtp.password) + '@' + encodeURIComponent(smtp.server) + ':' + smtp.port;
-  
-    Accounts.emailTemplates = {
-      from: 'RWGB <noreply@rwgb.de>',
-      siteName: 'RWGB',
-      verifyEmail: {
-        subject: function(user) {
-          return 'Emailbestätigung für rwgb.de';
-        },
-        text: function(user, url) {
-          return 'Hi,\n' +
-            'Rufe diesen Link auf um deine Email-Adresse für dein RWGB-Konto zu bestätigen:\n' + url +
-            '\n\nWenn du mit dieser Email nichts anfangen kannst, ignoriere sie einfach.' +
-            '\n\nGruuuß';
-        }
-      },
-      resetPassword: {
-        subject: function(user) {
-          return 'Passwort neu setzen für rwgb.de';
-        },
-        text: function(user, url) {
-          return 'Hi,\n' +
-            'Rufe diesen Link auf um ein neues Passwort für dein RWGB-Konto zu setzen:\n' + url +
-            '\n\nWenn du mit dieser Email nichts anfangen kannst, ignoriere sie einfach.' +
-            '\n\nGruuuß';
-        }
-      }
-    };
-
-    Accounts.onCreateUser(function(options, user) {
-      Meteor.setTimeout(function() {
-        Accounts.sendVerificationEmail(user._id);
-      }, 2 * 1000);
-      return user;
-    });
-  });
-
   Meteor.publish('entrys', function (limit) {
     return Entrys.find({}, {
       limit: limit,
